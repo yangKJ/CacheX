109855770b2c1277442fbe065f22504865e5018c
Condy <yangkj310@gmail.com>
2023-10-27 11:20:51 +0800

    Pod 0.1.0

--- CacheXDemo/ViewController.swift
+++ CacheXDemo/ViewController.swift
@@ -12,11 +12,10 @@ class ViewController: UIViewController {
     
     lazy var imageView: UIImageView = {
         let width = self.view.bounds.size.width-40
-        let rect = CGRect(x: 20, y: 100, width: width, height: width)
+        let rect = CGRect(x: 20, y: 200, width: width, height: width)
         let imageView = UIImageView(frame: rect)
-        imageView.center = self.view.center
         imageView.layer.cornerRadius = 10
-        imageView.layer.borderColor = UIColor.green.cgColor
+        imageView.layer.borderColor = UIColor.red.withAlphaComponent(0.5).cgColor
         imageView.layer.borderWidth = 2
         imageView.layer.masksToBounds = true
         imageView.contentMode = .scaleAspectFit
@@ -27,6 +26,7 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.frame = CGRect(x: 20, y: imageView.frame.maxY + 20, width: 200, height: 20)
         label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 20)
         return label
     }()
     
